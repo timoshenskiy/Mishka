eabf8bad87424ad8e7ded016faf332eea840fe12
Alexandr <timoshenskiy.ad@gmail.com>
2020-08-25 21:29:29 +0300

    исправление недочетов + адаптивная графика

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -1,23 +1,22 @@
-var HIDE_CLASS = "visually-hidden";
+var HIDE_CLASS = 'visually-hidden';
+var NAVIGATION_CLOSE_CLASS = 'header__navigation--close';
+var SEARCH_AND_CART_CLOSE_CLASS = 'header__search-cart-wrapper--close';
+var MENU_BUTTON_CLOSE_CLASS = 'header__menu-button--close';
 
-var NAVIGATION = document.querySelector(".header__navigation");
-var NAVIGATION_CLOSE_CLASS = "header__navigation--close";
 
-var SEARCH_AND_CART = document.querySelector(".header__search-cart-wrapper");
-var SEARCH_AND_CART_CLOSE_CLASS = "header__search-cart-wrapper--close";
+var navigation = document.querySelector('.header__navigation');
+var searchAndCart = document.querySelector('.header__search-cart-wrapper');
+var menuButton = document.querySelector('.header__menu-button');
 
-var MENU_BUTTON = document.querySelector(".header__menu-button");
-MENU_BUTTON.classList.remove(HIDE_CLASS);
-var MENU_BUTTON_CLOSE_CLASS = "header__menu-button--close";
-
-NAVIGATION.classList.add(NAVIGATION_CLOSE_CLASS);
-SEARCH_AND_CART.classList.add(SEARCH_AND_CART_CLOSE_CLASS);
-MENU_BUTTON.classList.add(MENU_BUTTON_CLOSE_CLASS);
+menuButton.classList.remove(HIDE_CLASS);
+navigation.classList.add(NAVIGATION_CLOSE_CLASS);
+searchAndCart.classList.add(SEARCH_AND_CART_CLOSE_CLASS);
+menuButton.classList.add(MENU_BUTTON_CLOSE_CLASS);
 
 var menuButtonClickHandler = function () {
-  NAVIGATION.classList.toggle(NAVIGATION_CLOSE_CLASS);
-  SEARCH_AND_CART.classList.toggle(SEARCH_AND_CART_CLOSE_CLASS);
-  MENU_BUTTON.classList.toggle(MENU_BUTTON_CLOSE_CLASS);
+  navigation.classList.toggle(NAVIGATION_CLOSE_CLASS);
+  searchAndCart.classList.toggle(SEARCH_AND_CART_CLOSE_CLASS);
+  menuButton.classList.toggle(MENU_BUTTON_CLOSE_CLASS);
 }
 
-MENU_BUTTON.addEventListener("click", menuButtonClickHandler)
+menuButton.addEventListener('click', menuButtonClickHandler)
--- source/js/popup.js
+++ source/js/popup.js
@@ -1,26 +1,61 @@
-var HIDE_CLASS = 'visually-hidden';
-var SHOW_CLASS = 'popup-show';
+var ESC_KEY = 27;
+
+var HIDE_CLASS = 'cart-popup--close';
+var SHOW_CLASS = 'cart-popup__wrapper--animated';
 var CART_POPUP_SELECTOR = '.cart-popup';
 var CART_POPUP_WRAPPER_SELECTOR = '.cart-popup__wrapper';
 var CART_POPUP_CONTROLS_SELECTOR = '.size-controls__radio';
-var ORDER_BUTTON_SELECTOR = '.order-js';
+var CART_BUTTON_SELECTOR = '.catalog__button';
+var ORDER_BUTTON_SELECTOR = '.week-offer__order-link';
 
 var cartPopup = document.querySelector(CART_POPUP_SELECTOR);
 var cartPopupWrapper = document.querySelector(CART_POPUP_WRAPPER_SELECTOR);
-var orderButtons = document.querySelectorAll(ORDER_BUTTON_SELECTOR);
-var cartPopupControls = document.querySelectorAll(CART_POPUP_CONTROLS_SELECTOR);
 
-if (orderButtons) {
-  for (var i = 0; i < orderButtons.length; i++) {
-    orderButtons[i].addEventListener('click', function(evt) {
-      evt.preventDefault();
-      cartPopup.classList.remove(HIDE_CLASS);
-      cartPopupWrapper.classList.add(SHOW_CLASS);
+var orderButton = document.querySelector(ORDER_BUTTON_SELECTOR);
+var cartButtons = document.querySelectorAll(CART_BUTTON_SELECTOR);
 
-      for (var j = 0; j < cartPopupControls.length; j++) {
-        cartPopupControls[j].setAttribute("tabindex", 0);
-      }
-      cartPopupControls[0].focus();
-    });
+var controls = document.querySelector(CART_POPUP_CONTROLS_SELECTOR);
+
+var closePopup = function () {
+  cartPopup.classList.add(HIDE_CLASS);
+  cartPopupWrapper.classList.remove(SHOW_CLASS);
+  cartPopup.removeEventListener('click', cartBackgroundClickHandler)
+  document.removeEventListener('keydown', escapeButtonKeydownHandler);
+}
+
+var openPopup = function () {
+  cartPopup.classList.remove(HIDE_CLASS);
+  cartPopupWrapper.classList.add(SHOW_CLASS);
+  controls.focus();
+  cartPopup.addEventListener('click', cartBackgroundClickHandler)
+  document.addEventListener('keydown', escapeButtonKeydownHandler);
+}
+
+var orderButtonClickHandler = function (evt) {
+  evt.preventDefault();
+      openPopup();
+}
+
+var cartBackgroundClickHandler = function (evt) {
+  if(cartPopup === evt.target) {
+    closePopup();
   }
 }
+
+var escapeButtonKeydownHandler = function (evt) {
+  if (evt.keyCode === ESC_KEY) {
+    closePopup();
+  }
+}
+
+/* index.html */
+if (orderButton) {
+    orderButton.addEventListener('click', orderButtonClickHandler);
+};
+
+/* catalog.html */
+if (cartButtons) {
+  for (var i = 0; i < cartButtons.length; i++) {
+    cartButtons[i].addEventListener('click', orderButtonClickHandler);
+  }
+};
